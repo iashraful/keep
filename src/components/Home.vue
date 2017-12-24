008268d9f7c7b0cad9c11f56e4b22a41e6b6b033
Ashraful Islam <ashrafulrobin3@gmail.com>
2017-12-24 16:20:20 +0600

    Removed vue initially created files

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="home">
+        <h1 class="text-center">{{mgs}}</h1>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: "Home",
+        data() {
+            return {
+                mgs: "I'm from Home"
+            }
+        }
+    }
+</script>
+
+<style>
+    .text-center {
+        text-align: center
+    }
+</style>

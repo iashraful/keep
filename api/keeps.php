d97e19ae37b9c23886ebb4ca743bfb99ea531da3
Ashraful Islam <ashrafulrobin3@gmail.com>
2018-01-18 17:35:57 +0600

    Added list notes function and handle GET request

--- api/keeps.php
+++ api/keeps.php
@@ -2,6 +2,31 @@
 
 header('Content-Type: application/json');
 
+function list_notes() {
+    // Open a MySQL connection
+    include "config/db_config.php";
+    $query = "SELECT * FROM `keeps` ORDER BY id DESC";
+    $result = $conn->query($query);
+    $json_response_array = array();
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $row_array['id'] = $row['id'];
+            $row_array['note'] = $row['note'];
+            $row_array['is_checked'] = $row['is_checked'];
+            $row_array['created_at'] = $row['created_at'];
+            $row_array['updated_at'] = $row['updated_at'];
+            array_push($json_response_array, $row_array);
+        }
+        return $json_response_array;
+    }
+    else{
+        $response = [];
+        return $response;
+    }
+    // Close Connection
+    mysqli_close($conn);
+}
+
 function add_note($note) {
     // Open a MySQL connection
     include "config/db_config.php";
@@ -15,7 +40,7 @@ function add_note($note) {
     	echo json_encode($response);
     }
     // Close Connection
-    $conn.close();
+    mysqli_close($conn);
 }
 
 function update_note($id, $note, $checked=0) {
@@ -35,7 +60,25 @@ function update_note($id, $note, $checked=0) {
     	echo json_encode($response);
     }
     // Close Connection
-    $conn.close();
+    mysqli_close($conn);
+}
+
+
+// Handle HTTP requests like, GET, POST, PUT
+
+if($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $response = list_notes();
+    echo json_encode($response);
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $response = ["status" => "[POST] Under Construction."];
+    echo json_encode($response);
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'PUT') {
+    $response = ["status" => "[PUT] Under Construction."];
+    echo json_encode($response);
 }
 
 ?>

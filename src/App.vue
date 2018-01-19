64abec0a81c21a27ab3d16f423c028e06262f680
Ashraful Islam <ashrafulrobin3@gmail.com>
2018-01-19 15:00:41 +0600

    * Added navbar * Added Login Page design * separed html and css for login page

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
   <div id="app">
+  	<ul>
+	  <li class="brand"><a href="/#/">Ashraful's Keep</a></li>
+	  <li>
+	  	<div class="search">
+		    <input class="search-input" type="text" placeholder="Search.." name="search">
+		 </div>
+	  </li>
+	  <li><a class="active" href="/#/login">Login</a></li>
+	</ul>
     <router-view/>
   </div>
 </template>
@@ -16,6 +25,43 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+/* Navbar Design */
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+    float: right;
+}
+.brand {
+	float: left;
+}
+.search {
+	position: absolute;
+    left: 38%;
+}
+.search-input {
+	width: 350px;
+	padding: 7px;
+  margin-top: 8px;
+  border: none;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #111;
 }
 </style>
---
+++ src/components/Login.vue
@@ -0,0 +1,15 @@
+<template src="../templates/login-template.html"></template>
+
+<script>
+	export default {
+		name: "Login",
+		data() {
+			return {
+				username: "",
+				password: ""
+			}
+		}
+	}
+</script>
+
+<style src="../styles/login-style.css"></style>

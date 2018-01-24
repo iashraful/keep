da804fd681c18c7af67ef39a8a4b4aa0d2261526
Ashraful Islam <ashrafulrobin3@gmail.com>
2018-01-24 16:17:10 +0600

    * Added Home page design * Added Add note functionality

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,8 +6,47 @@
         name: "Home",
         data() {
             return {
-                mgs: "I'm from Home"
+            		authenticated: true,
+                // authenticated: localStorage.getItem('login') == 'true' ? true : false
+                note: "",
+                notes: [],
             }
+        },
+        methods: {
+        	noteSubmit() {
+        		const apiUrl = '/api/keeps.php';
+						const payload = {
+							method: 'POST',
+							headers: {'Content-Type': 'application/json'},
+							body: JSON.stringify({
+								note: this.note
+							})
+						};
+						fetch(apiUrl, payload).then((response) => {
+							return response.json();
+						}).then((data) => {
+							if(data.status == 200) {
+								this.notes.unshift(data);
+							}
+						})
+        	},
+        	triggerCheckbox(id) {
+        		let isChecked = data.is_checked == 1 ? 0 : 1;
+        	}
+        },
+        created() {
+        	// This will trigger when this component will mount.
+        	// Here will fetch all the notes from server
+        	const apiUrl = '/api/keeps.php';
+					const payload = {
+						method: 'GET',
+						headers: {'Content-Type': 'application/json'},
+					};
+					fetch(apiUrl, payload).then((response) => {
+						return response.json();
+					}).then((data) => {
+						this.notes = data;
+					})
         }
     }
 </script>
